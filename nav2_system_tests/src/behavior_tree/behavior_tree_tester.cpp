61f9879c73e04dee0e1bfd06831a25adb68722af
Vinny Ruia <vruia@me.com>
2020-11-28 23:06:51 +0000

    checkpoint commit before I realized we don't need to create the action clients

---
+++ nav2_system_tests/src/behavior_tree/behavior_tree_tester.cpp
@@ -0,0 +1,85 @@
+// Copyright (c) 2020 Vinny Ruia
+// Copyright (c) 2020 Sarthak Mittal
+// Copyright (c) 2018 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License. Reserved.
+
+#include <string>
+#include <random>
+#include <tuple>
+#include <memory>
+#include <iostream>
+#include <chrono>
+#include <sstream>
+#include <iomanip>
+
+#include "behavior_tree_tester.hpp"
+
+using namespace std::chrono_literals;
+using namespace std::chrono;  // NOLINT
+
+namespace nav2_system_tests
+{
+
+BehaviorTreeTester::BehaviorTreeTester()
+: is_active_(false)
+{
+  node_ = rclcpp::Node::make_shared("behavior_tree_test");
+  
+  compute_path_to_pose_client_ptr_ = rclcpp_action::create_client("compute_path_to_pose")
+  follow_path_client_ptr_ = rclcpp_action::create_client("follow_path")
+  spin_client_ptr_ = rclcpp_action::create_client("spin")
+  wait_client_ptr_ = rclcpp_action::create_client("wait")
+  back_up_client_ptr_ = rclcpp_action::create_client("back_up")
+
+}
+
+BehaviorTreeTester::~BehaviorTreeTester()
+{
+    if (is_active_) {
+        deactivate();
+    }
+}
+
+void BehaviorTreeTester::activate()
+{
+    if (is_active_) {
+        throw std::runtime_error("Trying to activate while already activated");
+        return;
+    }
+
+    std::this_thread::sleep_for(10s);
+
+    //checkForActionClient
+
+    RCLCPP_INFO(this->node_->get_logger(), "All action servers are ready");
+    is_active_ = true;
+}
+
+void BehaviorTreeTester::deactivate()
+{
+    if(!is_active_) {
+        throw std::runtime_error("Trying to deactivate while already inactive");
+    }
+    is_active_ = false;
+}
+
+rclcpp_action::Client<ActionT>::SharedPtr BehaviorTreeTester::create_client(string clientName)
+{
+    return rclcpp_action::create_client<ActionT>(
+    node_->get_node_base_interface(),
+    node_->get_node_graph_interface(),
+    node_->get_node_logging_interface(),
+    node_->get_node_waitables_interface(),
+    clientName);
+}
--- nav2_system_tests/src/behavior_tree/behavior_tree_tester.hpp
+++ nav2_system_tests/src/behavior_tree/behavior_tree_tester.hpp
@@ -24,6 +24,7 @@
 #include "behaviortree_cpp_v3/bt_factory.h"
 
 #include "nav2_behavior_tree/test/test_action_server.hpp"
+#include "nav2_behavior_tree/test/test_service.hpp"
 
 #include "nav2_behavior_tree/plugins/action/compute_path_to_pose_action.hpp"
 #include "nav2_behavior_tree/plugins/action/follow_path_action.hpp"
@@ -34,6 +35,116 @@
 
 namespace nav2_system_tests
 {
+    template<class ActionT>
+    class FakeActionServer : public TestActionServer<ActionT>
+    {
+    public:
+      FakeActionServer()
+      : FakeActionServer("fake")
+      {}
+
+    protected:
+      void execute(
+        const typename std::shared_ptr<rclpp_action::ServerGoalHandle<ActionT>>
+        goal_handle)
+      override
+      {
+          ActionT::Result::SharedPtr result = std::make_shared<ActionT::Result>();
+          bool return_success = getReturnSuccess();
+          if (return_success) {
+              goal_handle->success(result);
+          } else {
+              goal_handle->abort(result);
+          }
+      }
+    };
+
+    class ClearEntireCostmapService : public TestService<nav2_msgs::srv::ClearEntireCostmap>
+    {
+    public:
+    ClearEntireCostmapService()
+    : TestService("clear_entire_costmap")
+    {}
+    };
+
+    class ComputePathToPoseActionServer : public FakeActionServer<nav2_msgs::action::ComputePathToPose>
+    {
+      public:
+        ComputePathToPoseActionServer() : FakeActionServer("ComputePathToPoseActionServer")
+        {}
+    }
+    
+    class FollowPathActionServer : public FakeActionServer<nav2_msgs::action::FollowPath>
+    {
+      public:
+        FollowPathActionServer() : FakeActionServer("FollowPathActionServer")
+        {}
+    }
+    // Since Clear Costmap is a service not an action server, we will have to 
+    // figure out how to fake that
+    // class ClearCostmapActionServer : public FakeActionServer<nav2_msgs::action::ClearCostmap>
+    // {
+    //   public:
+    //     FollowPathActionServer() : FakeActionServer("FollowPathActionServer")
+    //     {}
+    // }
+    class SpinActionServer : public FakeActionServer<nav2_msgs::action::Spin>
+    {
+      public:
+        SpinActionServer() : FakeActionServer("SpinActionServer")
+        {}
+    }
+    
+    class WaitActionServer : public FakeActionServer<nav2_msgs::action::Wait>
+    {
+      public:
+        WaitActionServer() : FakeActionServer("WaitActionServer")
+        {}
+    }
+    
+    class BackUpActionServer : public FakeActionServer<nav2_msgs::action::BackUp>
+    {
+      public:
+        BackUpActionServer() : FakeActionServer("BackUpActionServer")
+        {}
+    }
+
+    class BehaviorTreeTester
+    {
+      public:
+        using ComputePathToPose = nav2_msgs::action::ComputePathToPose;
+        using FollowPath = nav2_msgs::action::FollowPath;
+        using Spin = nav2_msgs::action::Spin;
+        using Wait = nav2_msgs::action::Wait;
+        using BackUp = nav2_msgs::action::BackUp;
+
+        BehaviorTreeTester();
+        ~BehaviorTreeTester();
+
+        void activate();
+
+        void deactivate();
+      
+        bool isActive() const
+        {
+          return is_active_;
+        }
+      private:
+        bool is_active_;
+
+        rclcpp::Node::SharedPtr node_;
+
+        // Action Client to call action (that will be faked)
+        rclcpp_action::Client<ComputePathToPose>::SharedPtr compute_path_to_pose_client_ptr_;
+        rclcpp_action::Client<FollowPath>::SharedPtr follow_path_client_ptr_;
+        rclcpp_action::Client<Spin>::SharedPtr spin_client_ptr_;
+        rclcpp_action::Client<Wait>::SharedPtr wait_client_ptr_;
+        rclcpp_action::Client<BackUp>::SharedPtr back_up_client_ptr_;
+        // Do I need to create a "client" for the Clear CostMap service
+
+        template <class ActionT>
+        rclcpp_action::Client<ActionT>::SharedPtr create_client(string clientName);
+    }
 
 }  // namespace nav2_system_tests
 
